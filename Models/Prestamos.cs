bdca79660671f01dea360a4a9b734b8814a1a308
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-09-30 18:28:03 -0400

    Modelo realizado

---
+++ Models/Prestamos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlainaGarcia_Ap1_P1.Models;
+
+public class Prestamos
+{
+    [Key]
+    public int PrestamosId { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public string Deudor {  get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public string Concepto { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public double Monto { get; set; }
+}
