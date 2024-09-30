2ecb2225e6c59b25a7fc2230d2b1609c27e6d5cd
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-09-30 18:44:08 -0400

    Cambio en el modelo utilizado en el contexto, realizado

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -7,6 +7,6 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
-    public DbSet<Registros> Registros { get; set; } 
+    public DbSet<Prestamos> Prestamos { get; set; } 
 
 }
