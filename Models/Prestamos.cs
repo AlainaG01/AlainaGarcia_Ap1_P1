8615bb66df1f613e78e69bbc75a56894e5de2039
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-10-01 17:34:37 -0400

    Expresiones regulares agregadas y corregidas

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -9,11 +9,14 @@ public class Prestamos
     public int PrestamosId { get; set; }
 
     [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
     public string Deudor {  get; set; }
 
     [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
     public string Concepto { get; set; }
 
     [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten numeros enteros o decimales")]
     public double Monto { get; set; }
 }
