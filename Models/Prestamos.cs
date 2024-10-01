5c4105024663fced3550f092d3e6e2efb8deeab5
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-09-30 21:22:45 -0400

    Agregue las expresiones regulares

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -4,13 +4,16 @@ namespace AlainaGarcia_Ap1_P1.Models;
 
 public class Prestamos
 {
+
     [Key]
     public int PrestamosId { get; set; }
 
     [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression("^[a-zA-Z/s]+$", ErrorMessage= "Solo letras no caracteres especiales")]
     public string Deudor {  get; set; }
 
     [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression("^[a-zA-Z/s]+$", ErrorMessage = "Solo letras no caracteres especiales")]
     public string Concepto { get; set; }
 
     [Required(ErrorMessage = "Campo obligatorio")]
