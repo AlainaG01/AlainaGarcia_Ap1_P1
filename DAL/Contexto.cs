5270bbb9bb2bb7f4e562387604001b0467c6887d
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-10-07 23:55:42 -0400

    OnModelCreating del modelo Deudor agregado

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -9,4 +9,17 @@ public class Contexto : DbContext
 
     public DbSet<Prestamos> Prestamos { get; set; } 
 
+    public DbSet<Deudores> Deudores { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Deudores>().HasData(new List<Deudores>()
+        {
+            new Deudores(){DeudorId=1, Nombres="Juan Perez"},
+            new Deudores(){DeudorId=2, Nombres="Alina Garcia"},
+            new Deudores(){DeudorId=3, Nombres="Erian Brito"}
+        });
+    }
+
 }
