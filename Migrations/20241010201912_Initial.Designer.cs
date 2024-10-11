ced49e41c831308ae5377da7377736327808b28f
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-10-11 01:45:37 -0400

    Migracion y base de datos actualizadas

--- Migrations/20241010201912_Initial.Designer.cs
+++ Migrations/20241010201912_Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AlainaGarcia_Ap1_P1.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AlainaGarcia_Ap1_P1.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20241010201912_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
