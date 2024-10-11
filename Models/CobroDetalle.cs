3b39b19293a5bcdbb0f9184cdb8b801733425b35
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-10-11 01:43:45 -0400

    Modelo CobroDetalle realizado

---
+++ Models/CobroDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlainaGarcia_Ap1_P1.Models;
+
+public class CobroDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [ForeignKey("CobroId")]
+    public int CobroId { get; set; }
+    public Cobros? Cobro { get; set; }
+
+    [ForeignKey("PrestamoId")]
+    public int PrestamoId { get; set; }
+    public Prestamos? Prestamo { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten numeros enteros o decimales")]
+    public double ValorCobrado { get; set; }
+}
